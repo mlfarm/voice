9aad85e53597cc9815bf62f8c5b5102fcd07289b
jyori112 <jyori112@gmail.com>
2016-03-18 14:01:30 -0400

    add layer

--- recoder.py
+++ recoder.py
@@ -141,8 +141,8 @@ def learn(datapath):
     print("Encoding First Layer")
     enc = np.ndarray(shape=(d.shape[0], 64), dtype=np.float32)
     for i in range(d.shape[0]):
-        print("D", d[i:i+1].shape)
-        x = chainer.Variable(np.asarray(d[i:i+1]))
+        print("D", [d[i]].shape)
+        x = chainer.Variable(np.asarray([d[i]]))
         enc[i] = model.encode1(x).data[0]
 
     print("Learning Second Layer")
