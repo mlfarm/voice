ac6b83dc301421aff78265148c61e1252e0fb485
jyori112 <jyori112@gmail.com>
2016-03-24 01:30:04 -0400

    per file

--- speaker_parse.py
+++ speaker_parse.py
@@ -6,11 +6,6 @@ files = os.listdir('speaker/raw')
 for f in files:
     basename = '.'.join(os.path.basename(f).split('.')[:-1])
 
-    voice.convert2wav(os.path.join('speaker/raw', f), os.path.join('speaker/wav', basename + '.wav'))
-
-for f in files:
-    basename = '.'.join(os.path.basename(f).split('.')[:-1])
-
     voice.convert2float(os.path.join('speaker/wav', basename + '.wav'), os.path.join('speaker/float', basename + '.float'))
 
 for f in files:
@@ -20,5 +15,5 @@ for f in files:
 
 for f in files:
     basename = '.'.join(os.path.basename(f).split('.')[:-1])
-    
+
     voice.encode(os.path.join('speaker/power', basename + '.power'), os.path.join('speaker/encode', basename))
